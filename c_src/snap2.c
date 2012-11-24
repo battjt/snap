a929d538735ebb49b93e379a30d1f8e0dcf0914b
Joe Batt <Joe@SolidDesign.net>
2012-11-23 21:28:48 -0500

    Working C version with threads.
    Signed-off-by: Joe Batt <Joe@SolidDesign.net>

--- c_src/snap2.c
+++ c_src/snap2.c
@@ -10,9 +10,14 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <pthread.h>
 #include <pthread_workqueue.h>
 
 typedef int boolean;
+
+/*
+ * Structure to represent a single snapshot.  This stuff could be global, but due to my upbringing I'm unable to use globals.
+ */
 struct snap {
 	char * hash_dir;
 	int verbosity;
@@ -21,14 +26,17 @@ struct snap {
 	pthread_workqueue_t queue;
 	pthread_mutex_t mutex;
 	pthread_cond_t cond;
-	int count;
+	int outstandingTasks;
 };
 
+/*
+ * Wrapper for fprintf(stderr... with a level for logging
+ */
 void msg(struct snap *env, int level, char *fmt, ...) {
 	if (env->log_level > level) {
 		va_list a;
 		va_start(a, fmt);
-		vprintf(fmt, a);
+		vfprintf(stderr, fmt, a);
 		va_end(a);
 	}
 }
@@ -38,15 +46,22 @@ char nibbleToHex(unsigned char b) {
 	return b > 9 ? (b + 'A' - 10) : (b + '0');
 }
 
-void to_hex(char hex[SHA_DIGEST_LENGTH * 2],
-		unsigned char hash[SHA_DIGEST_LENGTH]) {
+/*
+ * Convert a char* of data to a char* of hex digits.  The length of hex will be 2*x+1 (for NULL termination).
+ */
+void to_hex(int length, unsigned char hash[], char hex[]) {
 	int i, j;
-	for (i = 0, j = 0; i < SHA_DIGEST_LENGTH; i++, j += 2) {
-		hex[j] = nibbleToHex(hash[i] >> 4);
-		hex[j + 1] = nibbleToHex(hash[i]);
+	for (i = 0, j = 0; i < length; i++) {
+		hex[j++] = nibbleToHex(hash[i] >> 4);
+		hex[j++] = nibbleToHex(hash[i]);
 	}
+	// always NULL terminate your strings Java programmers.
+	hex[length * 2] = '\0';
 }
 
+/*
+ *  Copy the file to the repo returning the hash filename.  If it's already there, just return the name.
+ */
 void copyToRepo(struct snap *env, char * from, struct stat *from_stat,
 		char *target) {
 	msg(env, 10, "     copyToRepo  %s\n", from);
@@ -60,10 +75,10 @@ void copyToRepo(struct snap *env, char * from, struct stat *from_stat,
 	unsigned char hash[SHA_DIGEST_LENGTH];
 	SHA1(map, from_stat->st_size, hash);
 	munmap(map, from_stat->st_size);
-	char hex[SHA_DIGEST_LENGTH * 2];
-	to_hex(hex, hash);
+	char hex[SHA_DIGEST_LENGTH * 2 + 1];
+	to_hex(SHA_DIGEST_LENGTH, hash, hex);
 
-	sprintf(target, "%s/%s", env->hash_dir, hex);
+	strcat(strcat(strcpy(target, env->hash_dir), "/"), hex);
 	msg(env, 10, "     target %s\n", target);
 
 	struct stat stat_target;
@@ -90,6 +105,8 @@ void copyToRepo(struct snap *env, char * from, struct stat *from_stat,
 	}
 	close(from_fd);
 }
+
+/* allocate directory if it's not there */
 void ensure_dir(struct snap* env, char* dir) {
 	struct stat b;
 	if (stat(dir, &b) && errno == ENOENT) {
@@ -106,18 +123,18 @@ struct snapdir {
 
 void snap(struct snap *env, char* from, char *to);
 
-void incCount(struct snap* env, int c) {
-	pthread_mutex_lock(env->mutex, NULL );
-	env->count += c;
-	if (env->count == 0) {
-		pthread_cond_signal(env->cond);
+void incrementOutstandingTasks(struct snap* env, int c) {
+	pthread_mutex_lock(&env->mutex);
+	env->outstandingTasks += c;
+	msg(env, 5, "incCount: %d to %d\n", c, env->outstandingTasks);
+	if (env->outstandingTasks == 0) {
+		pthread_cond_signal(&env->cond);
 	}
-	pthread_mutex_unlock(env->mutex);
+	pthread_mutex_unlock(&env->mutex);
 }
 
-void snapdir(void *data) {
+void snapDir_task_fn(void *data) {
 	struct snapdir *d = (struct snapdir *) data;
-	incCount(d, 1);
 	// recurse
 	DIR* dir = opendir(d->from);
 	struct dirent* de;
@@ -126,23 +143,34 @@ void snapdir(void *data) {
 	ensure_dir(d->env, d->to);
 	while ((de = readdir(dir)) != NULL ) {
 		if (strncmp(".", de->d_name, 2) && strncmp("..", de->d_name, 3)) {
-			sprintf(to_new, "%s/%s", d->to, de->d_name);
-			sprintf(from_new, "%s/%s", d->from, de->d_name);
+			strcat(strcat(strcpy(to_new, d->to), "/"), de->d_name);
+			strcat(strcat(strcpy(from_new, d->from), "/"), de->d_name);
 			snap(d->env, from_new, to_new);
 		}
 	}
-	incCount(d->env, -1);
+	incrementOutstandingTasks(d->env, -1);
+	free(d->from);
+	free(d->to);
+	free(d);
 }
-
+/*
+ * Meat of project.  Copy from into the hash repo and make a link from the label directory.
+ */
 void snap(struct snap *env, char* from, char *to) {
 	msg(env, 5, "snap from %s to %s\n", from, to);
 	struct stat statb;
 	stat(from, &statb);
 	if (S_IFDIR & statb.st_mode) {
+		// It's a directory, so put it on the work queue for a thread to do it.
+		incrementOutstandingTasks(env, 1);
 		msg(env, 8, "     dir  %s\n", from);
-		struct snapdir *d = malloc(sizeof(struct snapdir));
-		*d = (struct snapdir ) { env, from, to };
-		pthread_workqueue_additem_np(env->queue, snapdir, d, NULL, NULL );
+		struct snapdir *data = malloc(sizeof(struct snapdir));
+		char *copy(char *s) {
+			return strcpy(malloc(strlen(s) + 1), s);
+		}
+		*data = (struct snapdir ) { env, copy(from), copy(to) };
+		pthread_workqueue_additem_np(env->queue, snapDir_task_fn, data, NULL,
+				NULL );
 	} else if (S_IFREG & statb.st_mode) {
 		char copy[strlen(env->hash_dir) + FILENAME_MAX + 2];
 		copyToRepo(env, from, &statb, copy);
@@ -160,11 +188,18 @@ int main(int argc, char *argv[]) {
 	}
 	struct snap env;
 	env.log_level = 100;
-	env.hash_dir = alloca(strlen(argv[1]) + strlen("/hash"));
+	env.hash_dir = alloca(strlen(argv[1]) + strlen("/hash")+1);
 	strcat(strcpy(env.hash_dir, argv[1]), "/hash");
 	env.try_to_link = 1;
-	pthread_mutex_init(&env.mutex, NULL );
-	pthread_cond_init(&env.cond, &env.mutex);
+	env.outstandingTasks = 0;
+	if (pthread_mutex_init(&env.mutex, NULL )) {
+		int e = errno;
+		msg(&env, 0, "error pthread_mutex_init: %s\n", strerror(e));
+	}
+	if (pthread_cond_init(&env.cond, NULL )) {
+		int e = errno;
+		msg(&env, 0, "error pthread_cond_init: %s\n", strerror(e));
+	}
 
 	ensure_dir(&env, argv[1]);
 	ensure_dir(&env, env.hash_dir);
@@ -174,14 +209,14 @@ int main(int argc, char *argv[]) {
 	char to[strlen(argv[1]) + strlen(argv[2]) + 2];
 	strcat(strcat(strcpy(to, argv[1]), "/"), argv[2]);
 	int i;
-	incCount(&env, 1);
 	for (i = 3; i < argc; i++) {
 		snap(&env, argv[i], to);
 	}
-	incCount(&env, -1);
-	// wait for all done
-	pthread_mutex_lock(&env.mutex);
-	pthread_cond_wait(&env.mutex, &env.cond);
-	pthread_mutex_unlock(&env.mutex);
+	if (env.outstandingTasks > 0) {
+		// wait for all done
+		pthread_mutex_lock(&env.mutex);
+		pthread_cond_wait(&env.cond, &env.mutex);
+		pthread_mutex_unlock(&env.mutex);
+	}
 	return 0;
 }
